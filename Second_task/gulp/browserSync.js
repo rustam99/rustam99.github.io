bbfa5215f63f12b370482facc5da3e16bf40b540
rustam <rustam_lord@inbox.ru>
2020-04-28 13:45:08 +0300

    complete second task

---
+++ Second_task/gulp/browserSync.js
@@ -0,0 +1,24 @@
+'use strict';
+
+export default function(gulp, plugins, args, config, taskTarget, browserSync) {
+  // BrowserSync
+  gulp.task('browserSync', (done) => {
+    browserSync.init({
+      open: args.open ? 'local' : false,
+      startPath: config.baseUrl,
+      port: config.port || 3005,
+      server: {
+        baseDir: taskTarget,
+        routes: (() => {
+          let routes = {};
+
+          // Map base URL to routes
+          routes[config.baseUrl] = taskTarget;
+
+          return routes;
+        })()
+      }
+    });
+    done();
+  });
+}
